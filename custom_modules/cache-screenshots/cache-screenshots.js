57c5948d88f15633c46a8e94c46e0444f6e11671
smarkdurbin <s@markdurb.in>
2018-03-28 14:15:18 -0400

    solved caching function issues

--- custom_modules/cache-screenshots/cache-screenshots.js
+++ custom_modules/cache-screenshots/cache-screenshots.js
@@ -10,50 +10,35 @@ const download = require('image-downloader')
 const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
 
- 
-// Download to a directory and save with an another filename
-// const options = {
-//   url: 'http://placehold.it/500x500/?text=500PX TEST IMAGE',
-//   dest: __dirname + '/../../public/_cache/_images/_viewer_screenshots/'        // Save to /path/to/dest/photo.jpg
-// }
- 
-// download.image(options)
-//   .then(({ filename, image }) => {
-//     console.log('File saved to', filename)
-//   }).catch((err) => {
-//     throw err
-//   })
-
 function cacheOne(viewerObj) {
-    // console.log(viewerObj);
-    return viewerObj;
+    console.log(viewerObj);
+    // Download to a directory and save with an another filename
+    // Download to a directory and save with an another filename
+        const options = {
+            //   url: 'http://placehold.it/1920x1080/?text=' + viewerObj.name,
+            url: screenshotsAPIPath + viewerObj.name,
+            dest: screenshotsCachePath + viewerObj.name + '.png'
+        }
+        download.image(options)
+            .then(({ filename, image }) => {
+                imagemin([filename], screenshotsCachePath, {
+                    plugins: [
+                        imageminPngquant({ quality: '65-80' })
+                    ]
+                }).then(files => {
+                    console.log(files);
+                });
+                console.log('File saved to', filename);
+            }).catch((err) => {
+                throw err
+            });
+    
 };
 
 function cacheAll(viewersObj) {
     // console.log(viewersObj);
     viewersObj.forEach(function(viewer) {
-        // Download to a directory and save with an another filename
-        const options = {
-        //   url: 'http://placehold.it/1920x1080/?text=' + viewer.name,
-          url: screenshotsAPIPath + viewer.name,
-          dest: screenshotsCachePath + viewer.name + '.png'
-        }
-        download.image(options)
-          .then(({ filename, image }) => {
-            console.log('File saved to', filename)
-          }).catch((err) => {
-            throw err
-          })
-        console.log(viewer.name);
-        console.log('\n');
-    });
-    imagemin([screenshotsCachePath], screenshotsCachePath, {
-        plugins: [
-            imageminPngquant({quality: '65-80'})
-        ]
-    }).then(files => {
-        console.log(files);
-        //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …] 
+        cacheOne(viewer);
     });
     return viewersObj;
 };
